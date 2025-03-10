8b8b8c69fb866364d14ea272f51fc3d13ebe50db
Viktor Potoki <viktor.potoki@gmail.com>
2025-03-10 10:57:12 +0200

    addedscroll

--- lib/sign_in_screen.dart
+++ lib/sign_in_screen.dart
@@ -12,59 +12,63 @@ class SignInScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    bool isKeyboardOpen = MediaQuery.of(context).viewInsets.bottom > 0;
-    double bottomPadding = isKeyboardOpen ? 0 : 44;
     return Scaffold(
       backgroundColor: Colors.cyan,
       body: BlocProvider(
         create: (_) => ValidationCubit(),
         child: BlocBuilder<ValidationCubit, Validation>(
           builder: (context, state) {
-            return Center(
-              child: Padding(
-                padding: const EdgeInsets.all(32.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 32),
-                      child: CustomTextField(
-                        isPassword: false,
-                        errorText: state.emailErrorMessage,
-                        onChangedText: (value) {
-                          context.read<ValidationCubit>().emailText = value;
-                        },
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 68.0),
-                      child: CustomTextField(
-                        isPassword: true,
-                        errorText: state.passwordErrorMessage,
-                        onChangedText: (value) {
-                          context.read<ValidationCubit>().passwordText = value;
-                        },
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(bottom: bottomPadding),
-                      child: SizedBox(
-                        width: double.infinity,
-                        height: 48,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            context.read<ValidationCubit>().emailValidator();
-                            context.read<ValidationCubit>().passwordValidator();
-                            context.read<ValidationCubit>().onUpdated();
+            return SingleChildScrollView(
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(32.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      SizedBox(height: 400),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 32),
+                        child: CustomTextField(
+                          isPassword: false,
+                          errorText: state.emailErrorMessage,
+                          onChangedText: (value) {
+                            context.read<ValidationCubit>().emailText = value;
                           },
-                          child: Text(
-                            "Sing in",
-                            style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 68.0),
+                        child: CustomTextField(
+                          isPassword: true,
+                          errorText: state.passwordErrorMessage,
+                          onChangedText: (value) {
+                            context.read<ValidationCubit>().passwordText =
+                                value;
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 44),
+                        child: SizedBox(
+                          width: double.infinity,
+                          height: 48,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              context.read<ValidationCubit>().emailValidator();
+                              context
+                                  .read<ValidationCubit>()
+                                  .passwordValidator();
+                              context.read<ValidationCubit>().onUpdated();
+                            },
+                            child: Text(
+                              "Sing in",
+                              style: TextStyle(fontSize: 18),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             );
